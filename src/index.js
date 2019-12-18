7145c688c79e21e457c17b5fdec7a9a1d28c33c5
Kobo <kaboom2012@mail.ru>
2019-12-18 20:22:57 +0300

    brain-even upd

--- src/index.js
+++ src/index.js
@@ -27,16 +27,32 @@ export const brainEvenGame = () => {
   const userName = getName();
   console.log(`Hello, ${userName}!`);
   const randomNumber = Math.floor(Math.random() * 100);
+  const randomNumberTwo = Math.floor(Math.random() * 100);
+  const randomNumberThree = Math.floor(Math.random() * 100);
 
-  for (let i = 0; i <= 2; i += 1) {
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    if ((answer !== 'yes' && randomNumber % 2 === 0) || (answer !== 'no' && randomNumber % 2 !== 0)) {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      return console.log(`Let's try again, ${userName}!`);
-    }
-    console.log('Correct!');
+  console.log(`Question: ${randomNumber}`);
+  const answer = readlineSync.question('Your answer: ');
+  if ((answer !== 'yes' && randomNumber % 2 === 0) || (answer !== 'no' && randomNumber % 2 !== 0)) {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    return console.log(`Let's try again, ${userName}!`);
   }
+  console.log('Correct!');
+
+  console.log(`Question: ${randomNumberTwo}`);
+  const answerTwo = readlineSync.question('Your answer: ');
+  if ((answerTwo !== 'yes' && randomNumberTwo % 2 === 0) || (answerTwo !== 'no' && randomNumberTwo % 2 !== 0)) {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    return console.log(`Let's try again, ${userName}!`);
+  }
+  console.log('Correct!');
+
+  console.log(`Question: ${randomNumberThree}`);
+  const answerThree = readlineSync.question('Your answer: ');
+  if ((answerThree !== 'yes' && randomNumberThree % 2 === 0) || (answerThree !== 'no' && randomNumberThree % 2 !== 0)) {
+    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    return console.log(`Let's try again, ${userName}!`);
+  }
+  console.log('Correct!');
 
   return console.log('Congratulations!');
 };
