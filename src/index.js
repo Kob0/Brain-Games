3d343f422806ca0b916a6c1ce1fe0cb11fcd4c94
Kobo <kaboom2012@mail.ru>
2019-12-18 20:11:27 +0300

    brain-even upd

--- src/index.js
+++ src/index.js
@@ -28,15 +28,16 @@ export const brainEvenGame = () => {
   console.log(`Hello, ${userName}!`);
   const randomNumber = Math.floor(Math.random() * 100);
 
-  console.log(`Question: ${randomNumber}`);
-  const answer = readlineSync.question('Your answer: ');
-  if ((answer !== 'yes' && randomNumber % 2 === 0) || (answer !== 'no' && randomNumber % 2 !== 0)) {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${userName}!`);
-  } else {
-    console.log('Correct!');
+  for (let i = 0; i <= 2; i += 1) {
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if ((answer !== 'yes' && randomNumber % 2 === 0) || (answer !== 'no' && randomNumber % 2 !== 0)) {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${userName}!`);
+    } else {
+      console.log('Correct!');
+    }
   }
-
 
   return userName;
 };
