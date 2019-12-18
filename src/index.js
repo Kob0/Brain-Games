fe16a19fc9d3974efae1b52c7c49cac307399e03
Kobo <kaboom2012@mail.ru>
2019-12-18 20:31:46 +0300

    brain-even upd

--- src/index.js
+++ src/index.js
@@ -32,7 +32,11 @@ export const brainEvenGame = () => {
 
   console.log(`Question: ${randomNumber}`);
   const answer = readlineSync.question('Your answer: ');
-  if ((answer !== 'yes' && randomNumber % 2 === 0) || (answer !== 'no' && randomNumber % 2 !== 0)) {
+  if (answer !== 'yes' && randomNumber % 2 === 0) {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    return console.log(`Let's try again, ${userName}!`);
+  }
+  if (answer !== 'no' && randomNumber % 2 !== 0) {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     return console.log(`Let's try again, ${userName}!`);
   }
@@ -40,7 +44,11 @@ export const brainEvenGame = () => {
 
   console.log(`Question: ${randomNumberTwo}`);
   const answerTwo = readlineSync.question('Your answer: ');
-  if ((answerTwo !== 'yes' && randomNumberTwo % 2 === 0) || (answerTwo !== 'no' && randomNumberTwo % 2 !== 0)) {
+  if (answerTwo !== 'yes' && randomNumberTwo % 2 === 0) {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    return console.log(`Let's try again, ${userName}!`);
+  }
+  if (answerTwo !== 'no' && randomNumberTwo % 2 !== 0) {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     return console.log(`Let's try again, ${userName}!`);
   }
@@ -48,7 +56,11 @@ export const brainEvenGame = () => {
 
   console.log(`Question: ${randomNumberThree}`);
   const answerThree = readlineSync.question('Your answer: ');
-  if ((answerThree !== 'yes' && randomNumberThree % 2 === 0) || (answerThree !== 'no' && randomNumberThree % 2 !== 0)) {
+  if (answerThree !== 'yes' && randomNumberThree % 2 === 0) {
+    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+    return console.log(`Let's try again, ${userName}!`);
+  }
+  if (answerThree !== 'no' && randomNumberThree % 2 !== 0) {
     console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
     return console.log(`Let's try again, ${userName}!`);
   }
