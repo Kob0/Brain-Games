e0e58f2480c8a641541f5221a4b0c686dc9bb2bc
Kobo <kaboom2012@mail.ru>
2019-12-23 20:25:09 +0300

    brain-calc game upd

--- src/index.js
+++ src/index.js
@@ -45,3 +45,48 @@ export const brainEvenGame = () => {
 
   return console.log(`Congratulations, ${userName}!`);
 };
+
+const randomElement = (coll) => {
+  const randomized = Math.floor(Math.random() * coll.length);
+
+  return coll[randomized];
+};
+
+const expression = (num, num2, operation) => {
+  switch (operation) {
+    case '*':
+      return num * num2;
+    case '-':
+      return num - num2;
+    case '+':
+      return num + num2;
+    default:
+      return NaN;
+  }
+};
+
+export const greetingForBrainCalc = () => {
+  console.log('Welcome to the Brain Games!\nWhat is the result of the expression?');
+};
+
+export const brainCalcGame = () => {
+  const operations = ['*', '-', '+'];
+  const userName = getName();
+  console.log(`Hello, ${userName}!`);
+
+  for (let i = 0; i <= 2; i += 1) {
+    const randomOperation = randomElement(operations);
+    const randomNum = randomNumbers();
+    const randomNum2 = randomNumbers();
+    console.log(`Question: ${randomNum} ${randomOperation} ${randomNum2}`);
+    const correctAnswer = expression(randomNum, randomNum2, randomOperation);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (correctAnswer !== userAnswer) {
+      return console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.\nLet's try again, ${userName}!`);
+    }
+    console.log('Correct!');
+  }
+
+  return console.log(`Congratulations, ${userName}!`);
+};
