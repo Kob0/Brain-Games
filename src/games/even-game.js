8ede7fd5db5289355b1fef0121f7bafc6f21013c
Kobo <kaboom2012@mail.ru>
2019-12-26 21:03:51 +0300

    .

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -4,4 +4,3 @@ import { randomNumbers, isEven } from '../math';
 const gameGreeting = 'Answer "yes" if the number is even, otherwise answer "no".';
 const randomNumber = randomNumbers();
 const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-const question = () =>  
