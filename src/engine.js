d6d09965df3b864956728778238f3479ae60d6cb
Kobo <kaboom2012@mail.ru>
2020-01-11 17:33:32 +0300

    brain-calc game upd

--- src/engine.js
+++ src/engine.js
@@ -9,7 +9,7 @@ export const engine = (gameGreeting, question, correctAnswer) => {
   console.log(`Hello, ${userName}`);
 
   for (let i = 0; i < 3; i += 1) {
-    const userAnswer = readlineSync.question(`${question}: `);
+    const userAnswer = readlineSync.question(`${question()}: `);
     if (userAnswer !== correctAnswer) {
       return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${userName}!`);
     }
--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -6,8 +6,8 @@ const operations = ['*', '-', '+'];
 const randomNum = () => randomNumbers();
 const randomNum2 = () => randomNumbers();
 const randomOperation = () => randomElement(operations);
-const correctAnswer = String(expression(randomNum, randomNum2, randomOperation));
-const question = `Question: ${randomNum} ${randomOperation} ${randomNum2}`;
+const correctAnswer = () => String(expression(randomNum, randomNum2, randomOperation));
+const question = () => `Question: ${randomNum()} ${randomOperation()} ${randomNum2()}`;
 
 export default () => {
   const game = engine(gameGreeting, question, correctAnswer);
