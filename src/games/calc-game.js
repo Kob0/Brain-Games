2a0575aba26e92e9130c4a927bf6676b9b11b74f
Kobo <kaboom2012@mail.ru>
2020-01-16 11:46:09 +0300

    calc-game upd

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -3,13 +3,11 @@ import { expression, randomNumbers, randomElement } from '../math';
 
 const gameGreeting = 'What is the result of the expression?';
 const operations = ['*', '-', '+'];
-const randomNum = () => randomNumbers();
-const randomNum2 = () => randomNumbers();
 const randomOperation = () => randomElement(operations);
 
 const gameData = () => {
-  const num = randomNum();
-  const num2 = randomNum2();
+  const num = randomNumbers();
+  const num2 = randomNumbers();
   const operation = randomOperation();
   const question = `Question: ${num} ${operation} ${num2}`;
   const correctAnswer = String(expression(num, num2, operation));
