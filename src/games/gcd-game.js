8e59a878f0b732500c46cb40a5c97fa5f3903ab3
Kobo <kaboom2012@mail.ru>
2020-01-23 17:08:31 +0300

    brain-gcd game added

---
+++ src/games/gcd-game.js
@@ -0,0 +1,20 @@
+import { engine } from '../engine';
+import { randomNumbers, greaterCommonDivisor } from '../math';
+
+const gameGreeting = 'Find the greatest common divisor of given numbers.';
+
+const gameData = () => {
+  const num = randomNumbers();
+  const num2 = randomNumbers();
+  const question = `${num} ${num2}`;
+  const correctAnswer = greaterCommonDivisor(num, num2);
+  const data = [question, correctAnswer];
+
+  return data;
+};
+
+export default () => {
+  const game = engine(gameGreeting, gameData);
+
+  return game;
+};
