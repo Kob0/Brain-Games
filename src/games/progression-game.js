d434434bc0613301ec7bd7018815918b93196562
Kobo <kaboom2012@mail.ru>
2020-01-27 20:32:58 +0300

    progression game created

---
+++ src/games/progression-game.js
@@ -0,0 +1,30 @@
+import { engine } from '../engine';
+import { randomNumbers } from '../math';
+
+const gameGreeting = 'What number is missing in the progression?';
+
+const gameData = () => {
+  const progression = [];
+  const firstElement = randomNumbers(1, 100);
+  const progressionStep = randomNumbers(1, 100);
+  progression.push(firstElement);
+
+  for (let i = 1; i < 10; i += 1) {
+    progression[i] = progression[i - 1] + progressionStep;
+    progression.push(progression[i]);
+  }
+
+  const questionElement = randomNumbers(0, 9);
+  const correctAnswer = progression[questionElement];
+  progression[questionElement] = '..';
+  const question = progression.split(' ');
+  const data = [question, correctAnswer];
+
+  return data;
+};
+
+export default () => {
+  const game = engine(gameGreeting, gameData);
+
+  return game;
+};
