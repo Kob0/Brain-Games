c91b7c2a57434f5229639fd19398d861bc327af2
Kobo <kaboom2012@mail.ru>
2020-02-08 15:38:52 +0300

    rename 'greaterCommonDivisor' to 'getGreaterCommonDivisor'

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -3,10 +3,10 @@ import genRandomNumber from '../math';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-export const greaterCommonDivisor = (num, num2) => {
+export const getGreaterCommonDivisor = (num, num2) => {
   if (num2 > 0) {
     const div = num % num2;
-    return greaterCommonDivisor(num2, div);
+    return getGreaterCommonDivisor(num2, div);
   }
   return Math.abs(num);
 };
@@ -15,7 +15,7 @@ const getGameData = () => {
   const num = genRandomNumber(1, 100);
   const num2 = genRandomNumber(1, 100);
   const question = `${num} ${num2}`;
-  const correctAnswer = String(greaterCommonDivisor(num, num2));
+  const correctAnswer = String(getGreaterCommonDivisor(num, num2));
 
   return [question, correctAnswer];
 };
