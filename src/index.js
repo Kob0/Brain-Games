cca1918d447ad278bb08f40ad53faabde3024f61
Kobo <kaboom2012@mail.ru>
2020-02-10 16:51:07 +0300

    rename gameData to getGameData

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import readlineSync from 'readline-sync';
 
 const roundsCount = 3;
 
-export default (description, gameData) => {
+export default (description, getGameData) => {
   console.log(`Welcome to the Brain Games!\n${description}\n`);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}\n`);
 
   for (let i = 0; i < roundsCount; i += 1) {
-    const data = gameData();
+    const data = getGameData();
     const question = `Question: ${data[0]}`;
     const correctAnswer = data[1];
     const userAnswer = readlineSync.question(`${question}\nYour answer: `);
