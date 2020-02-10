fc46da3866a7a320656077a8ed38c5f4e148ada9
Kobo <kaboom2012@mail.ru>
2020-02-10 19:11:46 +0300

    refactor getGameData

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -2,13 +2,13 @@ import makeGame from '..';
 import genRandomNumber from '../math';
 
 const description = 'What number is missing in the progression?';
+const progressionLength = 10;
 
 const getGameData = () => {
   const progression = [];
   const firstElement = genRandomNumber(1, 100);
   const progressionStep = genRandomNumber(1, 100);
   progression.push(firstElement);
-  const progressionLength = 10;
 
   for (let i = 1; i < progressionLength; i += 1) {
     const element = progression[i - 1] + progressionStep;
