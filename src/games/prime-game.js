9b23d273403d8cd5ac68253b871ee335eb53e557
Kobo <kaboom2012@mail.ru>
2020-02-10 16:58:53 +0300

    delete export from isPrime func

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -3,7 +3,7 @@ import genRandomNumber from '../math';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-export const isPrime = (num) => {
+const isPrime = (num) => {
   if (num === 1) {
     return true;
   }
