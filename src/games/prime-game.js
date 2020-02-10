3978bc532cf840615709a3322107c8f6c1f58bd8
Kobo <kaboom2012@mail.ru>
2020-02-10 17:09:34 +0300

    refactor isPrime

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,7 +4,7 @@ import genRandomNumber from '../math';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num === 1) {
+  if (num < 2) {
     return true;
   }
 
