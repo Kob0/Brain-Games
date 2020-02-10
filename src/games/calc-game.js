0b64176efd6b6f72869f51013aad6df6fe36032d
Kobo <kaboom2012@mail.ru>
2020-02-10 17:41:42 +0300

    delete genRandomOperation func

--- src/games/calc-game.js
+++ src/games/calc-game.js
@@ -15,19 +15,13 @@ const calculateExpression = (num, num2, operation) => {
   }
 };
 
-const getRandomOperation = (operations) => {
-  const randomOperation = genRandomNumber(0, operations.length - 1);
-
-  return operations[randomOperation];
-};
-
 const description = 'What is the result of the expression?';
 const operations = ['*', '-', '+'];
 
 const getGameData = () => {
   const num = genRandomNumber(1, 20);
   const num2 = genRandomNumber(1, 15);
-  const operation = getRandomOperation(operations);
+  const operation = operations[genRandomNumber(0, operations.length - 1)];
   const question = `${num} ${operation} ${num2}`;
   const correctAnswer = String(calculateExpression(num, num2, operation));
 
